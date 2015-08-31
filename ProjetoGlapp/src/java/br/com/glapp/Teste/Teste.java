9b5eff411f7dd81a0a6f7055a2d1b2ba9e19def1
patricksales <pspuppim@gmail.com>
2015-08-31 12:13:24 -0300

    adicionado das bibliotecas e Classe de Teste

--- ProjetoGlapp/src/java/br/com/glapp/Teste/Teste.java
+++ ProjetoGlapp/src/java/br/com/glapp/Teste/Teste.java
@@ -1,5 +1,8 @@
 package br.com.glapp.Teste;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author Patrick
@@ -7,7 +10,8 @@ package br.com.glapp.Teste;
 public class Teste {
 
     public static void main(String args[]) {
-        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ProjetoGlappPU");
+        System.out.println("");
     }
 
 }
